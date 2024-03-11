1aa6f17057726f75778863d9f29439b61fb6516f
Av3001 <avneetwadhwa87@gmail.com>
2024-03-11 15:24:09 +0530

    bug fixes in building project

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -12,13 +12,14 @@ const page = () => {
       </h1>
       <div className="flex flex-wrap justify-center"> 
         {servicedata.services.map((service)=>(
-             <CardContainer className="inter-var m-4">
+             <CardContainer className="inter-var m-4" key={service.id}>
              <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]
               w-auto sm:w-[30rem] h-auto rounded-xl p-6 border "
               key={service.id}>
                <CardItem
                  translateZ="50"
                  className="text-xl font-bold text-neutral-600 dark:text-white"
+                 key={service.id}
                >
                  {service.type}
                </CardItem>
@@ -26,6 +27,7 @@ const page = () => {
                  as="p"
                  translateZ="60"
                  className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                 key={service.id}
                >
                 {service.description}
                </CardItem>
@@ -43,6 +45,7 @@ const page = () => {
                    translateZ={20}
                    as="button"
                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                   key={service.id}
                  >
                    Try now →
                  </CardItem>
@@ -50,6 +53,7 @@ const page = () => {
                    translateZ={20}
                    as="button"
                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                   key={service.id}
                  >
                    Sign up
                  </CardItem>
